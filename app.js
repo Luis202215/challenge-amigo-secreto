a849cc94b62fc1b575c5e1fe8513be97b955eac8
luis <luisalejandroquijada@gmail.com>
2025-02-03 01:11:47 -0500

    Creando Variable de tipo Array

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // El principal objetivo de este desafío es fortalecer
 //  tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Declarando variable de tipo array 
+// que almacenara nombre de amigos ingresados
+
+let amigos =[];
